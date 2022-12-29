04b66afab591e280115d04695ee5e17de4646ee1
Barbod Garsha <barbodgarshasbi79@gmail.com>
2022-12-29 10:52:43 +0100

    error handling is working pretty good

--- js/main.js
+++ js/main.js
@@ -14,6 +14,10 @@ viewer_MM = document.querySelector('[data-viewer-MM]')
 viewer_YY = document.querySelector('[data-viewer-YY]')
 viewer_CVC = document.querySelector('[data-viewer-CVC]')
 
+error_card_number = document.querySelector('[data-error-card-number]')
+error_date = document.querySelector('[data-error-date]')
+error_CVC = document.querySelector('[data-error-CVC]')
+
 // useful functions ==============================
 function onlyNumbers(str) {
     return /^[0-9]+$/.test(str);
@@ -21,6 +25,11 @@ function onlyNumbers(str) {
 
 // MAIN ==========================================
 inputs_field.addEventListener('input', function(e) {
+    error_card_number.classList.add('inputs__error--display-none')
+    error_date.classList.add('inputs__error--display-none')
+    error_CVC.classList.add('inputs__error--display-none')
+    
+   
     if(input_cardholder.value != "") {
         viewer_cardholder.innerHTML = input_cardholder.value;
     }
@@ -31,7 +40,11 @@ inputs_field.addEventListener('input', function(e) {
     if(input_card_number.value != "") {
         var n = input_card_number.value.replace(/\s/g, '') // delete all the spaces
 
-        if(!onlyNumbers(n)) { console.log("ERROR") } // TODO: show error for letters not allowed
+        if(!onlyNumbers(n)) { 
+            //if(!input_card_number.classList.contains('inputs__error--display-none')) 
+            error_card_number.classList.remove('inputs__error--display-none')
+        } // TODO: show error for letters not allowed
+        
         if(n.length > 16) {console.log('ERROR')} // too long
 
         n = n.replace(/\d{4}(?=.)/g, '$& ') // add a space inbetween every 4 number
@@ -42,27 +55,27 @@ inputs_field.addEventListener('input', function(e) {
         viewer_card_number.innerHTML = "0000 0000 0000 0000"
     }
     
-    if(input_MM.value != "") {
+    if(input_MM.value.replace(/\s/g, '') != "") {
         viewer_MM.innerHTML = input_MM.value;
     }
     else {
-        console.log("ERROR") // can't be black
+        error_date.classList.remove('inputs__error--display-none')
         viewer_MM.innerHTML = "00"
     }
     
-    if(input_YY.value != "") {
+    if(input_YY.value.replace(/\s/g, '') != "") {
         viewer_YY.innerHTML = input_YY.value;
     }
     else {
-        console.log("ERROR") // can't be black
+        error_date.classList.remove('inputs__error--display-none')
         viewer_YY.innerHTML = "00"
     }
     
-    if(input_CVC.value != "") {
+    if(input_CVC.value.replace(/\s/g, '') != "") {
         viewer_CVC.innerHTML = input_CVC.value;
     }
     else {
-        console.log("ERROR") // can't be black
+        error_CVC.classList.remove('inputs__error--display-none')
         viewer_CVC.innerHTML = "000"
     }
     
